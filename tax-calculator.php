c6bc72b483c8b66d39e469685970ed3bc77d9900
dawidwojcik476 <69675993+dawidwojcik476@users.noreply.github.com>
2021-09-16 13:54:10 +0200

    Update tax-calculator.php

--- tax-calculator.php
+++ tax-calculator.php
@@ -21,23 +21,13 @@ add_action('wp_enqueue_scripts', 'plugin_js');
 
 function tax_calculator()
 {
-?>
-    <div id="tax-calculator">
-        <taxcalculator
-        ></taxcalculator>
-    </div>
-<?php
-}; ?>
+require_once '/tax-calculator.html';
+}; 
 
-<?php add_shortcode('taxcalculato', 'tax_calculator');?>
+add_shortcode('taxcalculator', 'tax_calculator');
 
-
-<?php function calculator_post_type() {
-    
-
- 
-
-            register_taxonomy( 'calc_netto', ['calc'], array(
+function calculator_post_type() {
+    register_taxonomy( 'calc_netto', ['calc'], array(
                 'label'         => 'Kwota Netto',
                 'show_admin_column' => true,
                 'publicly_queryable' => false,
@@ -85,36 +75,24 @@ function tax_calculator()
 
 add_action('init', 'calculator_post_type');
 
-
-
 function create_post() {
-
     $curPageName = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);  
 
     if(isset($_POST['productname'])){
-
-       
-        
         $my_post = array(
                 'post_type' => 'calc',
                 'post_title' => $_POST['productname'],
                 'post_calc_netto' => $_POST['netto'],
-                'post_status' => 'publish', 
-                
+                'post_status' => 'publish',    
             );
-
-            
-        
-        
        $calc_post = wp_insert_post($my_post);
         wp_set_object_terms($calc_post, $_POST['netto'], 'calc_netto', false );
         wp_set_object_terms($calc_post, $_POST['brutto'], 'calc_brutto', false );
         wp_set_object_terms($calc_post, $_POST['taxresult'], 'calc_taxresult', false );
-        
+    
         wp_redirect($curPageName);
         
-        exit();
-      
+        exit();      
     }
 }
 add_action('init','create_post');
